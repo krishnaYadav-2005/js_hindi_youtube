e19b37fab9daa496e3b33e7eb878fdaa3f869bef
krishnaYadav-2005 <krishna.2430936@gmail.com>
2026-08-21 18:32:13 +0000

    datatypes_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,34 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // by default undefined initialise hota hai 
+
+const id = Symbol('123')
+const anotherId = Symbol('123') // symbol ka kaam hi yhi hai ki agar value bhi same pass karenge lekin answer different aayega
+
+console.log(id == anotherId);
+const bigNumber = 34567890954321997565n
+
+
+// Reference (Non primitive)
+
+// Array, onbject, functions
+
+const heroes = ["Shaktiman", "naagraj", "doga"]
+   let myObj =  {
+        name: "Krishna",// in value ko hum variable me bhi store kar sakte hai
+        age: 22,
+
+    }
+const myfunction = function() // function ko js me as a variable bhi declare kar sakte hai
+{
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+
+
