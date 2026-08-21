55e11745bdfbd37bc8752d30b9d3626b40254a45
krishnaYadav-2005 <krishna.2430936@gmail.com>
2026-08-21 16:50:52 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let score = null
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
 // console.log(typeof valueInNumber); //  number 
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = ""  //<- false
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0=> false
 // "" => false
@@ -23,10 +23,45 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **************************** Operations *************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2-2);
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
 
 
+console.log(true); // true
+console.log(+true); // 1
+let num1, num2, num3
+num1 = num2 = num3 = 2+2  // aise nhi karna ki padne me readibility na ho
 
+let gameCounter = 100
+++gameCounter; // 101
+gameCounter++; // 101
+console.log(gameCounter); // for study of postfix and prefix we can go postfix and prefix js mdn
 
+// link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
 
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+//is tarah ke conversion ko avoid karna chahiye and problem language inconsistency ki dikkat
+
+// ===
+console.log("2" === 2);
+
