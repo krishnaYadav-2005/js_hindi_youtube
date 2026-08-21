67b0d77254f1318fa547bc6da027520f92c71cdd
krishnaYadav-2005 <krishna.2430936@gmail.com>
2026-08-21 06:57:46 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Krishna");
+
+let name = "Krishna"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standlone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Krishna");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
+
+
